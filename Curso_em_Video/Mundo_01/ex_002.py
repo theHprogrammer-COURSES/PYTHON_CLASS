775aa7eaf0f664062d0c7b25f94251217bf5225f
theHprogrammer <helder.h.silva@grad.ufsc.br>
2023-03-05 14:21:06 -0300

    Add input method

--- Curso_em_Video/Mundo_01/ex_002.py
+++ Curso_em_Video/Mundo_01/ex_002.py
@@ -2,7 +2,7 @@
 
 # O método input() é usado para receber dados do usuário. Ele retorna uma string.
 # O método str() é usado para converter o valor recebido para string. Pode ser usado para garantir que o valor recebido é uma string.
-nome = input(str('Qual é o seu nome?'))
+nome = str(input('Qual é o seu nome?'))
 
 # O formato de string f permite que você insira variáveis ​​em uma string usando um par de chaves {}.
 print(f'Olá, {nome}, seja bem-vindo!')
