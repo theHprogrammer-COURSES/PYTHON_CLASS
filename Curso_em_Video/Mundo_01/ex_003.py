c45e68e45a5b8d9362264fd4405bf9a1cadd0477
theHprogrammer <helder.h.silva@grad.ufsc.br>
2023-03-05 14:33:55 -0300

    Add sum integer

---
+++ Curso_em_Video/Mundo_01/ex_003.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+soma = n1 + n2
+
+print(f'A soma entre {n1} e {n2} é {soma}')
